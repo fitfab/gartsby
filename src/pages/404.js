0798d452f0d376de543d117e92f9110e717d2073
Miguel Julio <mjulio@artnet.com>
2017-08-15 15:57:21 -0400

    grab the html.js from cache/default-html.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 
 const NotFoundPage = () =>
   <div>
+    <Helmet
+      title="Gatsby | 404"
+      meta={[
+        { name: 'description', content: '404 page for our Gatsby!' },
+        { name: 'keywords', content: '404, 404 page, Oops, Sorry, Not Found' },
+      ]}
+    />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
 
 const IndexPage = () =>
   <div>
+    <Helmet
+        title="Gatsby | Home"
+        meta={[
+          { name: 'description', content: 'Welcome ot the home page!' },
+          { name: 'keywords', content: 'reactjs, gatsby, webpack, Sass, Redux, Graphql' },
+        ]}
+      />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
